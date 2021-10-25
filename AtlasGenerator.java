ade5575fa3fcffbf1fd18dde6da95f46ee16739d
Adrvil <jakub.szyma99@gmail.com>
2021-10-25 21:10:39 +0200

    Style: Changed instruction

--- AtlasGenerator.java
+++ AtlasGenerator.java
@@ -7,7 +7,7 @@ public class AtlasGenerator {
 			System.out.println("\tUsage: AtlasGenerator <name> <width> <height> <padding> <border> <unitCoordinates> <directory>");
 			System.out.println("\t\t<padding>: Padding between images in the final texture atlas.");
 			System.out.println("\t\t<border>: Every image will have a 1 pixel border");
-			System.out.println("\t\t<unitCoordinates>: Coordinates will be written to atlas txt file in 0..1 range instead of 0..width, 0..height range");
+			System.out.println("\t\t<unitCoordinates>: Coordinates will be saved to atlas txt file in 0..1 range instead of 0..width, 0..height range");
 			System.out.println("\tExample: AtlasGenerator atlas 2048 2048 5 1 1 images");
 		}
 		
